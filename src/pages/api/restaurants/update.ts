6e152ccc132591987622bee7c556b1c16ae9bf57
ruda8711 <jojo@f-lab.kr>
2025-02-10 02:10:39 +0900

    fefef

--- src/pages/api/restaurants/update.ts
+++ src/pages/api/restaurants/update.ts
@@ -20,8 +20,8 @@ interface MyFunctionResult {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
       const myFunction = httpsCallable(functions, 'helloWorld2');
-      res.status(200).json({ message: 'data2' });
-      // const res2 = await myFunction();//({ token: 'value1', title: 'value2', body: 'value3' });
+      const res2 = await myFunction();//({ token: 'value1', title: 'value2', body: 'value3' });
+      res.status(200).json({ message: 'data3' });
       // const result = res2 as AxiosResponse<MyFunctionResult>;
       // const data = result.data as MyFunctionResult;
       // res.status(200).json({ message: data });
