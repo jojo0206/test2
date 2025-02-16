fd5fab870156e6420c8d0e1295f2e5ea39c03832
ruda8711 <jojo@f-lab.kr>
2025-02-17 03:07:02 +0900

    fefef

--- src/pages/api/alarms/update.ts
+++ src/pages/api/alarms/update.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getFirestore, doc, updateDoc } from 'firebase/firestore';
+import { collection, getFirestore, doc, updateDoc, addDoc } from 'firebase/firestore';
 import { firebaseApp } from '@/firebase/firebaseClient';
 
 // export interface Alarm {
@@ -23,14 +23,29 @@ import { firebaseApp } from '@/firebase/firebaseClient';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { id,name,sendType,sendUserIdList,contents, repeat, weekDays, hour, minute, imageUrl } = req.body.data;
-    try {
-      const db = getFirestore(firebaseApp!);
-      const docRef = doc(db, 'alarms', id);
-      await updateDoc(docRef, { contents, name, sendType, sendUserIdList, repeat, weekDays, hour, minute, imageUrl });
-      res.status(200).json({ message: 'Alarm updated Successfully'});
-    } catch (error) {
-      console.error("Error adding document: ", error);
-      res.status(500).json({ error: 'Error adding document' });
+    if (id == null || id == '') {
+      try {
+        const db = getFirestore(firebaseApp!);
+
+        const docRef = await addDoc(collection(db, "alarms"), { contents, name, sendType, sendUserIdList, repeat, weekDays, hour, minute, imageUrl });
+        
+        // const docRef = doc(db, 'alarms', id);
+        // await updateDoc(docRef, { contents, name, sendType, sendUserIdList, repeat, weekDays, hour, minute, imageUrl });
+        res.status(200).json({ message: 'Alarm updated Successfully'});
+      } catch (error) {
+        console.error("Error adding document: ", error);
+        res.status(500).json({ error: 'Error adding document' });
+      }
+    } else {
+      try {
+        const db = getFirestore(firebaseApp!);
+        const docRef = doc(db, 'alarms', id);
+        await updateDoc(docRef, { contents, name, sendType, sendUserIdList, repeat, weekDays, hour, minute, imageUrl });
+        res.status(200).json({ message: 'Alarm updated Successfully'});
+      } catch (error) {
+        console.error("Error adding document: ", error);
+        res.status(500).json({ error: 'Error adding document' });
+      }
     }
   }
 }
