b8c23c4fc3052b59cf83c3e05cdb7e0285d61683
ruda8711 <jojo@f-lab.kr>
2025-02-17 00:33:34 +0900

    d

--- src/pages/alarms/[id].tsx
+++ src/pages/alarms/[id].tsx
@@ -205,14 +205,14 @@ const AlarmDetailPage = ({alarm}: AlarmProps) => {
           <Input
             type="checkbox"
             id="isMon"
-            checked={isMon}
+            checked={isMon || false}
             onChange={handleIsMonChange}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
           <Input
             type="checkbox"
             id="isTue"
-            checked={isTue}
+            checked={isTue || false}
             onChange={handleIsTueChange}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
